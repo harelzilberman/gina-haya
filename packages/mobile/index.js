2d4cf921e7cb19d8bb1aa54ee7ee74bd887c6cf5
Harel Zilberman <harelzilberman@gmail.com>
2026-06-08 10:40:46 +0300

    fix: add FormData polyfill for Hermes

--- packages/mobile/index.js
+++ packages/mobile/index.js
@@ -1,4 +1,16 @@
 import './src/cryptoPolyfill';
+
+// FormData polyfill for Hermes
+if (typeof FormData === 'undefined') {
+  global.FormData = class FormData {
+    constructor() {
+      this.data = {};
+    }
+    append(key, value) { this.data[key] = value; }
+    get(key) { return this.data[key]; }
+  };
+}
+
 import { registerRootComponent } from 'expo';
 import App from './App';
 registerRootComponent(App);
